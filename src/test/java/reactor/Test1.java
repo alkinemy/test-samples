ae095516bdd21c29a168f0010bfba5bc04b3f244
joke111 <alta.second@gmail.com>
2018-11-21 01:38:33 +0900

    reactor test

--- src/test/java/reactor/Test1.java
+++ src/test/java/reactor/Test1.java
@@ -3,6 +3,7 @@ package reactor;
 import org.junit.Test;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+import reactor.core.scheduler.Scheduler;
 import reactor.core.scheduler.Schedulers;
 
 import java.time.Duration;
@@ -12,25 +13,29 @@ import java.util.List;
 public class Test1 {
 
 	@Test
-	public void test() {
+	public void test() throws InterruptedException {
+
+		Scheduler scheduler = Schedulers.newElastic("test");
 
 		long start = System.currentTimeMillis();
 		List<Integer> integers = Flux.just(1, 2, 3, 4, 5, 6, 7)
 			.flatMap(i -> Mono.fromCallable(() -> getResult(i))
-				.timeout(Duration.ofSeconds(5))
-				.onErrorResume(e -> Mono.just(new Result(0)))
-				.doOnNext(r -> System.out.println(Thread.currentThread().getName() + ": " + r.getI()))
-				.subscribeOn(Schedulers.elastic()))
+				.doOnError(e -> System.out.println("Fail1: " + e.getMessage() + ", " + e.getClass()))
+				.onErrorResume(e -> Mono.empty())
+				.timeout(Duration.ofSeconds(5), Mono.empty())
+				.subscribeOn(scheduler)
+			)
+			.doOnNext(r -> System.out.println(Thread.currentThread().getName() + ": " + r.getI()))
 			.map(Result::square)
-			.take(Duration.ofSeconds(5))
-			.sort(Comparator.comparingInt(i -> i))
 			.filter(s -> s % 2 == 1)
+			.sort(Comparator.comparingInt(i -> i))
 			.collectList()
 			.block();
 
 		long end = System.currentTimeMillis();
 
 		System.out.println("time: " + (end - start) + ", result: " + integers);
+//		Thread.sleep(Integer.MAX_VALUE);
 
 	}
 
@@ -40,8 +45,11 @@ public class Test1 {
 				Thread.sleep(3000);
 				return new Result(i);
 			} catch (Exception e) {
-				throw new IllegalStateException("1");
+				throw new IllegalStateException("1", e);
 			}
+		}
+		if (i == 3) {
+			throw new TestException("3");
 		}
 		if (i == 4) {
 			try {
@@ -49,12 +57,23 @@ public class Test1 {
 				System.out.println("test: " + i);
 				return new Result(i);
 			} catch (Exception e) {
-				throw new IllegalStateException("4");
+				throw new IllegalStateException("4", e);
 			}
 		}
 		return new Result(i);
 	}
 
+}
+
+class TestException extends RuntimeException {
+
+	public TestException(String message) {
+		super(message);
+	}
+
+	public TestException(String message, Throwable cause) {
+		super(message, cause);
+	}
 }
 
 class Result {
