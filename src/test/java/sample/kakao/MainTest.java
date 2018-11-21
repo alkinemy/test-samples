729c7cf3a1f00570afab2d9570e50d01e7895540
joke111 <alta.second@gmail.com>
2018-11-22 02:32:37 +0900

    reactor scheduler sample

--- src/test/java/sample/kakao/MainTest.java
+++ src/test/java/sample/kakao/MainTest.java
@@ -3,6 +3,7 @@ package sample.kakao;
 import org.junit.Test;
 import reactor.core.publisher.DirectProcessor;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 import reactor.core.scheduler.Scheduler;
 import reactor.core.scheduler.Schedulers;
 
@@ -36,17 +37,18 @@ public class MainTest {
 	public void test2() throws InterruptedException {
 		Scheduler s = Schedulers.newParallel("parallel-scheduler", 4);
 
-		Flux<String> flux = Flux
-			.range(1, 10)
-			.map(i -> 10 + i)
-			.doOnNext(i -> System.out.println(i + " | INITIAL: " + Thread.currentThread().getName()))
-//			.flatMap(i -> Mono.fromCallable(() -> i + " | ZERO: " + Thread.currentThread().getName()).subscribeOn(s))
-			.map(i -> i + " | FIRST: " + Thread.currentThread().getName())
-			.map(i -> i + " | SECOND: " + Thread.currentThread().getName())
-//			.publishOn(s)
-//			.subscribeOn(s)
-			.map(i -> i + " | THIRD: " + Thread.currentThread().getName())
-			.map(i -> i + " | FOURTH: " + Thread.currentThread().getName())
+		Flux<String> flux =
+			Flux
+				.range(1, 10)
+				.map(i -> 10 + i)
+				.doOnNext(i -> System.out.println(i + " | INITIAL: " + Thread.currentThread().getName()))
+				.flatMap(i -> Mono.fromCallable(() -> i + " | ZERO: " + Thread.currentThread().getName()).subscribeOn(s))
+				.map(i -> i + " | FIRST: " + Thread.currentThread().getName())
+				.map(i -> i + " | SECOND: " + Thread.currentThread().getName())
+//				.publishOn(s)
+				.subscribeOn(s)
+				.map(i -> i + " | THIRD: " + Thread.currentThread().getName())
+				.map(i -> i + " | FOURTH: " + Thread.currentThread().getName())
 			;
 
 		new Thread(() -> flux.subscribe(i -> System.out.println(i + " | FIFTH: " + Thread.currentThread().getName()))).start();
